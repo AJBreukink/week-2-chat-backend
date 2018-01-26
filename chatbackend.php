c9889f368691b2614a15a6b5c90e41cb7d8a1375
Arend-Jan Breukink <35037565+AJBreukink@users.noreply.github.com>
2018-01-26 15:52:17 +0100

    Add files via upload
    vanuit simpel chatappje, POST naar en GET uit txt bestandje

--- chatbackend.php
+++ chatbackend.php
@@ -1,36 +1,36 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+$verb = $_SERVER['REQUEST_METHOD'];
 
-$myfile = fopen("thefile.txt", "a");
-$name = $_GET["username"];
-$message = $_GET["usermsg"];
-echo $name.$message;
-fwrite($myfile, $name.$message."\n");
-fclose($myfile);
+/*if ($verb =='PUT'){
 
+  $putdata = fopen("php://input", "r");
+  parse_str(file_get_contents("php://input"),$post_vars);
+  $writefile = fopen("thefile.txt", "a+");
+  $old_content = file_get_contents($mytextfile);
+  $new_content = $post_vars['mykey'].$post_vars['value']. "\n";
+  fwrite($mytextfile, $new_content."\n".$old_content);
+  fclose($mytextfile);
+*/
+if ($verb == 'POST') {
 
-/*if(isset($_GET['username'], $_GET['usermsg']))
-    {
-  global $myfile;
-  $name = $_GET["username"];
-  $message = $_GET["usermsg"];
-  fwrite($myfile, $name.$message."\n");
-  fclose($myfile);
-}*/
+  if(isset($_GET['mykey'], $_GET['value']))
+      {
+    $writefile = fopen("thefile.txt", "a");
+    $name = $_GET["mykey"];
+    $message = $_GET["value"];
+    fwrite($writefile, $name.": ".$message."\n");
+    fclose($writefile);
+      }
 
-/*
-    if(!($fp=fopen("thefile.txt","r")))
-    	{
-    		echo '<p class="style">Problem in opening the file!</p>';
-    	}
-    	else
-    	{
-    		while(!feof($fp))
-    		{
-    			$str=fgets($fp);
-    			echo $str."<br>";
-    		}
-    	}
-    	fclose($fp);
-      */
+    } elseif ($verb=='GET') {
+      if(isset($_GET['mykey'], $_GET['value'])) {
+      $readfile=fopen("thefile.txt", "r");
+      while(! feof($readfile)){
+          echo fgets($readfile). "<br />";
+          }
+        }
+      }
+
  ?>
